c3ce1f29f5cb3a505ae8aaee60712d0d7b73dd0f
shagu <ericmauser@googlemail.com>
2023-12-12 09:29:29 +0100

    ShaguCopy: change framestrata to medium

--- ShaguCopy.lua
+++ ShaguCopy.lua
@@ -8,21 +8,21 @@ for i=1,NUM_CHAT_WINDOWS do
 
   local HookAddMessage = ChatFrame.AddMessage
 
-  ChatFrame.scrollframe = CreateFrame('ScrollFrame', 'nChatCopyScroll', ChatFrame, 'UIPanelScrollFrameTemplate') 
+  ChatFrame.scrollframe = CreateFrame('ScrollFrame', 'ShaguChatCopyScroll', ChatFrame, 'UIPanelScrollFrameTemplate')
   ChatFrame.scrollframe:SetParent(ChatFrame)
   ChatFrame.scrollframe:SetAllPoints(ChatFrame)
   ChatFrame.scrollframe:SetScrollChild(ChatFrame.scrollframe.chatCopy)
   ChatFrame.scrollframe:Hide()
-  ChatFrame.scrollframe:SetScript("OnUpdate", function() 
-    this:UpdateScrollChildRect() 
+  ChatFrame.scrollframe:SetScript("OnUpdate", function()
+    this:UpdateScrollChildRect()
   end)
 
-  ChatFrame.scrollframe:SetScript("OnShow", function() 
-    this:SetScrollChild(ChatFrame.scrollframe.chatCopy) 
+  ChatFrame.scrollframe:SetScript("OnShow", function()
+    this:SetScrollChild(ChatFrame.scrollframe.chatCopy)
     ChatFrame.scrollframe.chatCopy:SetAllPoints(ChatFrame.scrollframe)
     this:SetVerticalScroll(this:GetVerticalScrollRange())
   end)
-  
+
   ChatFrame.scrollframe.bg = ChatFrame.scrollframe:CreateTexture(nil, "BACKGROUND")
   ChatFrame.scrollframe.bg:SetTexture(.1,.1,.1,1)
   ChatFrame.scrollframe.bg:SetPoint("TOPLEFT", ChatFrame.scrollframe, "TOPLEFT", -3, 3)
@@ -36,7 +36,7 @@ for i=1,NUM_CHAT_WINDOWS do
   ChatFrame.scrollframe.chatCopy:SetTextColor(1,1.1,1)
   ChatFrame.scrollframe.chatCopy:SetMultiLine(true)
 
-  local _, fontSize, _ = GetChatWindowInfo(i);
+  local _, fontSize, _ = GetChatWindowInfo(i)
 
   ChatFrame.scrollframe.chatCopy:SetFontObject(ChatFontNormal)
   ChatFrame.scrollframe.chatCopy:SetAutoFocus(false)
@@ -47,12 +47,12 @@ for i=1,NUM_CHAT_WINDOWS do
   end)
     ChatFrame.scrollframe.chatCopy:SetScript("OnEditFocusGained", function ()
   end)
-  
+
   ChatFrame.scrollframe:SetScrollChild(ChatFrame.scrollframe.chatCopy)
 
   ChatFrame.chatCopyButton = CreateFrame("Button", nil, ChatFrame)
   ChatFrame.chatCopyButton:SetPoint("TOPRIGHT", -3, -3)
-  ChatFrame.chatCopyButton:SetFrameStrata("TOOLTIP")
+  ChatFrame.chatCopyButton:SetFrameStrata("MEDIUM")
   ChatFrame.chatCopyButton:SetWidth(16)
   ChatFrame.chatCopyButton:SetHeight(16)
   ChatFrame.chatCopyButton:SetAlpha(.25)
@@ -60,12 +60,13 @@ for i=1,NUM_CHAT_WINDOWS do
   ChatFrame.chatCopyButton.icon:SetTexture("Interface\\AddOns\\ShaguCopy\\button")
   ChatFrame.chatCopyButton.icon:SetAllPoints(ChatFrame.chatCopyButton)
   ChatFrame.chatCopyButton:SetScript("OnClick", function(self)
-    if ChatFrame.scrollframe:IsShown() then 
+    if ChatFrame.scrollframe:IsShown() then
       ChatFrame.scrollframe:Hide()
-    else 
+    else
       ChatFrame.scrollframe:Show()
     end
   end)
+
   ChatFrame.chatCopyButton:SetScript("OnEnter", function(self)
     this:SetAlpha(1)
   end)
